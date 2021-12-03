0437dc7b1285c2f149a9747274fc3b05f745078c
vinodh1988 <k.c.vinodh@hotmail.com>
2021-12-03 16:27:30 +0530

    context-save-commit

--- src/ReactComponents/About.tsx
+++ src/ReactComponents/About.tsx
@@ -1,13 +1,15 @@
+import { useState } from "react"
 import { Context, reactStore} from "../Context/ContextAPI"
 import DataUnit from "./DataUnit"
 import MessageUnit from "./MessageUnit"
 
 
 const About=()=>{
+    const [message,setMessage]=useState(reactStore.message)
     return(
         <div className="mt-4 p-5 bg-success text-white rounded">
                 <h1>Contextual Demo</h1>
-                <Context.Provider value={{...reactStore}} >
+                <Context.Provider value={{...reactStore,message: message, changeMessage: setMessage}} >
                         <DataUnit></DataUnit>
                         <MessageUnit></MessageUnit>
                 </Context.Provider>
--- src/ReactComponents/DataUnit.tsx
+++ src/ReactComponents/DataUnit.tsx
@@ -1,15 +1,20 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Context } from "../Context/ContextAPI";
 
 const DataUnit=()=>{
-    const {fruits}=useContext(Context)
+    const {fruits,changeMessage}=useContext(Context)
+    const [temp,setTemp]=useState("")
+
     return (
 
         <div>
             <h3>Fruits</h3>
         <ul>
             {fruits.map((x:string)=><li>{x}</li>)}
+           
         </ul>
+        <input type="text" value={temp} onChange={e=>{setTemp(e.target.value)}}/>
+        <button onClick={()=>{changeMessage(temp)}}>addMessage</button>
         </div>
     )
 }
